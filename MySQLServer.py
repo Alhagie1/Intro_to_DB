069721ed274f5a507f106a90cae87f43f07e6753
alhagie nyang <alhagienyang13@gmail.com>
2025-08-23 12:47:06 +0000

    Creating a database in MySQL server

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,8 +1,14 @@
+# -- Creating a database in MySQL server
 
-# Creating a database in a MySQL server
+# The python mysql connector
 import mysql.connector
-# Main Script Function
+
+#  -- Main function for creating databases
 def create_database():
+    """
+    Connects to the MySQL server and creates a database named 'alx_book_store'
+    if it does not already exist.
+    """
     db_connection = None
     cursor = None
 
@@ -11,26 +17,25 @@ def create_database():
             host = "localhost",
             user = "root",
             port = 3306,
-            passwd = "mummy12.com"
-    )
-        cursor = db_connection.cursor()
-        
-        db_name = "alx_book_store"
+            password = "mummy12.com"
+        )
 
+        cursor = db_connection.cursor()
+
+        db_name = "alx_book_store"
         sql_query = f"CREATE DATABASE IF NOT EXISTS {db_name};"
-        
         cursor.execute(sql_query)
         print(f"Database '{db_name}' created successfully!")
-    # The exception block   
+
     except mysql.connector.Error as err:
         print(f"Error: {err}")
-            
+
     finally:
         if cursor:
             cursor.close()
         if db_connection and db_connection.is_connected():
             db_connection.close()
-            print("Database closed")
+            print("Database connection closed.")
 
 
 if __name__ == "__main__":
