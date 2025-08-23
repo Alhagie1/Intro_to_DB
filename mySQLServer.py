8ea8c6f40b8fc2779f92abebaa4db1eff7033c80
alhagie nyang <alhagienyang13@gmail.com>
2025-08-23 10:26:12 +0000

    Creating a database in mySQL server

---
+++ mySQLServer.py
@@ -0,0 +1,37 @@
+
+import mysql.connector
+
+# --- Main Script Logic 
+def create_database():
+    db_connection = None
+    cursor = None
+
+    try:
+        db_connection = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            port = 3306,
+            passwd = "mummy12.com"
+    )
+        cursor = db_connection.cursor()
+        
+        db_name = "alx_book_store"
+
+        sql_query = f"CREATE DATABASE IF NOT EXISTS {db_name};"
+        
+        cursor.execute(sql_query)
+        print(f"Database '{db_name}' created successfully!")
+        
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+            
+    finally:
+        if cursor:
+            cursor.close()
+        if db_connection and db_connection.is_connected():
+            db_connection.close()
+            print("Database connection closed.")
+
+
+if __name__ == "__main__":
+    create_database()
---
+++ task_2.sql
@@ -0,0 +1,46 @@
+
+
+-- A Bookstore database system
+
+
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+-- The Authors table
+CREATE TABLE Authors(
+    author_id INT PRIMARY KEY, -- This is the primary key
+    author_name VARCHAR(215)
+
+);
+
+-- The Books table
+CREATE TABLE IF NOT EXISTS Books(
+    book_id INT PRIMARY KEY, -- This is the primary key
+    title VARCHAR(130),
+    author_id INT, -- This is the foreign key
+    FOREIGN KEY(author_id) REFERENCES Authors(author_id),
+    price DOUBLE,
+    publication_date DATE
+);
+--  The customers table
+CREATE TABLE IF NOT EXISTS Customers(
+    customer_id  INT PRIMARY KEY, --This is the primary key
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+-- The Orders table
+CREATE TABLE IF NOT EXISTS Orders(
+    order_id INT PRIMARY KEY, --This is the primary key
+    customer_id INT, -- This is the foreign key
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_date DATE
+);
+ 
+--  The Order Details table
+CREATE TABLE IF NOT EXISTS Order_Details(
+    orderdetailid INT PRIMARY KEY, --This is the primary key
+    order_id INT, -- This the foreign key
+    book_id INT , -- This is a foreign key
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE
+);
